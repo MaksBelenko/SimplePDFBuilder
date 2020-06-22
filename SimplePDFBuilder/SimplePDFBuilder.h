6f69d5acb1a2482297427b5651679039b793007a
MaksBelenko <maksim.belenko@gmail.com>
2020-06-22 10:42:28 +0100

    Initial Commit

---
+++ SimplePDFBuilder/SimplePDFBuilder.h
@@ -0,0 +1,19 @@
+//
+//  SimplePDFBuilder.h
+//  SimplePDFBuilder
+//
+//  Created by Maksim on 22/06/2020.
+//  Copyright © 2020 Maksim Belenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimplePDFBuilder.
+FOUNDATION_EXPORT double SimplePDFBuilderVersionNumber;
+
+//! Project version string for SimplePDFBuilder.
+FOUNDATION_EXPORT const unsigned char SimplePDFBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplePDFBuilder/PublicHeader.h>
+
+
